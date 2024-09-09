12b75be2bcc8061e89e5f457258ad7f20d9ab9ef
sannidhayax1 <158429803+sannidhayax1@users.noreply.github.com>
2024-09-09 13:46:42 +0530

    Add files via upload

---
+++ spotify clone by sannidhaya Sharma/scrippt.js
@@ -0,0 +1,38 @@
+console.log("Let's write JavaScript!");
+
+async function getSongs(){
+    let songs = []; 
+    try {
+        let a = await fetch("http://127.0.0.1:3000/songs/");
+        let response = await a.text(); 
+        console.log(response);
+        
+        let div = document.createElement("div");
+        div.innerHTML = response;
+        
+        let as = div.getElementsByTagName("a");
+        for(let index = 0; index < as.length; index++){
+            const element = as[index];
+            if(element.href.endsWith(".mp3")){
+                songs.push(element.href);  
+            }
+        }
+    } catch (error) {
+        console.error("Error fetching songs:", error);
+    }
+    return songs;
+}
+
+async function main(){
+    let songs = await getSongs();
+    console.log(songs);
+
+    if (songs.length > 0) {
+        var audio = new Audio(songs[0]);
+        audio.play(); 
+    } else {
+        console.log("No songs found.");
+    }
+}
+
+main();
